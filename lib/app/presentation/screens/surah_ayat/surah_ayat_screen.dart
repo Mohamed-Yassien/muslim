8463b9634bba53e6b361ce3896ad65c95c48b8fa
Mohamed Yassien <moyassien219@gmail.com>
2023-09-25 07:47:12 +0300

    show surahs and surah ayat

--- lib/app/presentation/screens/surah_ayat/surah_ayat_screen.dart
+++ lib/app/presentation/screens/surah_ayat/surah_ayat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:muslin/app/presentation/components/custom_text.dart';
 import 'package:muslin/app/presentation/components/loading_and_error.dart';
+import 'package:muslin/app/presentation/components/to_arabic_converter.dart';
 
 import '../../components/surah_arabic_number.dart';
 import 'cubit/surah_ayat_cubit.dart';
@@ -41,10 +42,12 @@ class _SurahAyatScreenState extends State<SurahAyatScreen> {
             child: ListView.separated(
               padding: const EdgeInsets.all(15),
               itemBuilder: (context, index) {
-                final text = cubit.surahAyat[index];
-                final formattedText = index == 0 ? text : '\n$text';
+                final text = cubit.surahAyat.join(
+                  "\uFD3F${(index + 1).toString().toArabicNumbers}\uFD3E",
+                );
+
                 return CustomText(
-                  text: formattedText,
+                  text: text,
                   textColor: Colors.black,
                   textAlign: TextAlign.justify,
                 );
