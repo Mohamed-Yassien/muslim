21349b3bb84023cf62a9a31ae2a6da008b9b9b19
MAC ITADVIP <macitadvip@MACs-iMac.local>
2023-09-26 02:48:14 -0700

    quraan

--- lib/app/presentation/screens/surah_ayat/surah_ayat_screen.dart
+++ lib/app/presentation/screens/surah_ayat/surah_ayat_screen.dart
@@ -2,68 +2,11 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:muslin/app/presentation/components/custom_text.dart';
+import 'package:muslin/app/presentation/components/loading_and_error.dart';
 import 'package:muslin/app/presentation/components/surah_arabic_number.dart';
+import 'package:muslin/core/constants.dart';
 import 'package:quran/quran.dart';
 import 'cubit/surah_ayat_cubit.dart';
-
-// class SurahAyatScreen extends StatefulWidget {
-//   const SurahAyatScreen({
-//     Key? key,
-//     required this.surahNumber,
-//   }) : super(key: key);
-
-//   final int surahNumber;
-
-//   @override
-//   State<SurahAyatScreen> createState() => _SurahAyatScreenState();
-// }
-
-// class _SurahAyatScreenState extends State<SurahAyatScreen> {
-//   @override
-//   void initState() {
-//     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-//       var cubit = SurahAyatCubit.get(context);
-//       await cubit.loadSurahAyat(surahNumber: widget.surahNumber);
-//     });
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(),
-//       body: BlocBuilder<SurahAyatCubit, SurahAyatState>(
-//         builder: (context, state) {
-//           var cubit = SurahAyatCubit.get(context);
-//           return LoadingAndError(
-//             isError: state is GetSurahAyatErrorState,
-//             isLoading: state is GetSurahAyatLoadingState,
-//             child: ListView.separated(
-//               padding: const EdgeInsets.all(15),
-//               itemBuilder: (context, index) {
-//                 final text = cubit.surahAyat.join(
-//                   "\uFD3F${(index + 1).toString().toArabicNumbers}\uFD3E",
-//                 );
-
-//                 return CustomText(
-//                   text: text,
-//                   textColor: Colors.black,
-//                   textAlign: TextAlign.justify,
-//                 );
-//               },
-//               separatorBuilder: (context, index) {
-//                 return ArabicSuraNumber(
-//                   i: index,
-//                 );
-//               },
-//               itemCount: cubit.surahAyat.length,
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
 
 class SurahAyatScreen extends StatefulWidget {
   static const String id = "/SurahAyatScreen";
@@ -93,10 +36,13 @@ class _SurahAyatScreenState extends State<SurahAyatScreen> {
       appBar: AppBar(),
       body: BlocBuilder<SurahAyatCubit, SurahAyatState>(
         builder: (context, state) {
-          //   var cubit = SurahAyatCubit.get(context);
-          return Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: BodyOfSurah(surahNumber: surahIndex - 1),
+          return LoadingAndError(
+            isError: state is GetSurahAyatErrorState,
+            isLoading: state is GetSurahAyatLoadingState,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: BodyOfSurah(surahNumber: surahIndex - 1),
+            ),
           );
         },
       ),
@@ -125,30 +71,49 @@ class BodyOfSurah extends StatelessWidget {
               const SizedBox(
                 height: 5,
               ),
-              RichText(
-                textAlign: TextAlign.justify, // count of surah
-                text: TextSpan(
-                  children: [
-                    for (var i = 0;
-                        i <= SurahAyatCubit.get(context).surahAyat.length - 1;
-                        i++) ...{
-                      TextSpan(
-                        recognizer: TapGestureRecognizer()..onTap = () {},
-                        text: SurahAyatCubit.get(context).surahAyat[i],
-                        style: const TextStyle(
-                          fontFamily: 'Traditional',
-                          fontSize: 24,
-                          color: Colors.black87,
-                          height: 2,
+              GestureDetector(
+                onLongPress: () {},
+                child: RichText(
+                  textAlign: TextAlign.justify, // count of surah
+                  text: TextSpan(
+                    children: [
+                      for (var i = 0;
+                          i <= SurahAyatCubit.get(context).surahAyat.length - 1;
+                          i++) ...{
+                        TextSpan(
+                          recognizer: LongPressGestureRecognizer()
+                            ..onLongPress = () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  duration: Duration(seconds: 1),
+                                  backgroundColor: AppConstance.primaryColor,
+                                  elevation: 2,
+                                  content: CustomText(
+                                    text: "تم حفظ الاية بنجاح",
+                                    fontSize: 20,
+                                    textColor: Colors.white,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              );
+                            },
+                          text: SurahAyatCubit.get(context).surahAyat[i],
+                          style: const TextStyle(
+                            fontFamily: 'Traditional',
+                            fontSize: 24,
+                            color: Colors.black87,
+                            height: 2,
+                          ),
                         ),
-                      ),
-                      WidgetSpan(
+                        WidgetSpan(
                           alignment: PlaceholderAlignment.middle,
                           child: ArabicSuraNumber(
                             i: i,
-                          ))
-                    }
-                  ],
+                          ),
+                        ),
+                      }
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(
@@ -159,31 +124,6 @@ class BodyOfSurah extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class AyahSpanNumberWidget extends StatelessWidget {
-  const AyahSpanNumberWidget({
-    Key? key,
-    required this.i,
-  }) : super(key: key);
-
-  final int i;
-
-  @override
-  Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: 16,
-      child: Text(
-        // '$i',
-        getVerseEndSymbol(i, arabicNumeral: true),
-        textAlign: TextAlign.center,
-        style: Theme.of(context)
-            .textTheme
-            .bodyText2!
-            .copyWith(fontSize: 20, color: Colors.white),
-      ),
     );
   }
 }
